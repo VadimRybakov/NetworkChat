df5bd4afb170a0e1b96de0181c18d82f6a124100
Vadim Rybakov <vadimio9217@gmail.com>
2020-02-12 15:01:19 +0300

    add timeout close for not auth users

--- chat-server/src/core/ChatServer.java
+++ chat-server/src/core/ChatServer.java
@@ -10,6 +10,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
 public class ChatServer implements ServerSocketThreadListener, SocketThreadListener {
@@ -138,7 +140,8 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
         String nickname = SqlClient.getNickname(login, password);
         if (nickname == null) {
             putLog("Invalid login attempt: " + login);
-            client.authFail();
+            timeoutAuth(client);
+//            client.authFail();
             return;
         } else {
             ClientThread oldClient = findClientByNickname(nickname);
@@ -200,5 +203,17 @@ public class ChatServer implements ServerSocketThreadListener, SocketThreadListe
         return null;
     }
 
+    private void timeoutAuth(ClientThread client){
+        TimerTask timeoutClose = new TimerTask() {
+            @Override
+            public void run() {
+                client.authFail();
+                clients.remove(client);
+                putLog("Client is timed out");
+            }
+        };
+        Timer time = new Timer();
+        time.schedule(timeoutClose, 120000);
+    }
 
 }
