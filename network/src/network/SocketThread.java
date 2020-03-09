65d46a43f518c212b09618d393284ae460c8a6d3
Vadim Rybakov <vadimio9217@gmail.com>
2020-03-09 11:19:06 +0300

    without executors

--- network/src/network/SocketThread.java
+++ network/src/network/SocketThread.java
@@ -3,21 +3,18 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SocketThread extends Thread {
 
     private final SocketThreadListener listener;
     private final Socket socket;
     private DataOutputStream out;
-    private ExecutorService executorService = Executors.newCachedThreadPool();
 
     public SocketThread(SocketThreadListener listener, String name, Socket socket) {
         super(name);
         this.socket = socket;
         this.listener = listener;
-        executorService.execute(this);
+        start();
     }
 
     @Override
@@ -56,7 +53,7 @@ public class SocketThread extends Thread {
     }
 
     public synchronized void close() {
-        executorService.shutdown();
+        interrupt();
         try {
             socket.close();
         } catch (IOException e) {
